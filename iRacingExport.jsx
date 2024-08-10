d6016b6bf1ca5c238d798cc6daca7c2ebe099593
Matt Carroll <mwcarroll@gmail.com>
2024-08-10 16:40:08 -0400

    update export to allow for helmets and suits as well as handling general errors

--- iRacingExport.jsx
+++ iRacingExport.jsx
@@ -6,48 +6,84 @@
 
 homeDrive = $.getenv("HOMEDRIVE");
 homePath = $.getenv("HOMEPATH");
-paintPath = homeDrive + homePath + "\\Documents\\iRacing\\paint";
+var paintPath = homeDrive + homePath + "\\Documents\\iRacing\\paint";
+var specPath = "";
+var mainPath = "";
 
 doc = app.activeDocument;
 
 // details from filename
-carInfo = doc.name.split(".")[0].split("_")
-carModel = carInfo[0];
-carPath = paintPath + "\\" + carModel;
-carNum = carInfo[1].substring(0, 6);
+var isHelmet = false;
+var isSuit = false;
+
+var fileInfo = doc.name.split(".")[0].split("_")
+
+var model = fileInfo[0];
+var number = fileInfo[1].split("-")[0];
+
+switch(model){
+    case "helmet":
+        isHelmet = true;
+
+        mainPath = paintPath + "\\helmet_" + number;   
+        break;
+    case "suit":
+        isSuit = true;
+
+        mainPath = paintPath + "\\suit_" + number;   
+        break;
+    default:
+        paintPath = paintPath + "\\" + model; 
+        specPath = paintPath + "\\car_spec_" + number;
+        mainPath = paintPath + "\\car_" + number;
+
+        break;
+}
+
+var specLayer, paintableLayer, infoLayer;
 
 // layers
-specLayer = doc.layers["Custom Spec Map"];
-paintableLayer = doc.layers["Paintable Area"];
-infoLayer = doc.layers["Turn Off Before Exporting TGA"];
+try {
+    specLayer = doc.layers["Custom Spec Map"];
+} catch(e) {}
+
+try {
+    paintableLayer = doc.layers["Paintable Area"];
+} catch(e) {}
+
+try {
+    infoLayer = doc.layers["Turn Off Before Exporting TGA"];
+} catch(e) {}
 
 // store visibility of layers so that we can go back to how they were
-specVisible = specLayer.visible;
-paintableVisible = paintableLayer.visible;
-infoVisible = infoLayer.visible;
+specVisible = specLayer != undefined ? specLayer.visible : false;
+paintableVisible = paintableLayer != undefined ? paintableLayer.visible : false;
+infoVisible = infoLayer != undefined ? infoLayer.visible : false;
 
 // save spec layer
-specPath = carPath + "\\car_spec_" + carNum;
-paintableLayer.visible = false;
-infoLayer.visible = false;
-specLayer.visible = true;
-saveTarga(specPath);
+if(!isHelmet && !isSuit){
+    if(paintablelayer != undefined) paintableLayer.visible = false;
+    if(infoLayer != undefined) infoLayer.visible = false;
+    if(specLayer != undefined) specLayer.visible = true;
+
+    saveTarga(specPath);
+}
 
 // save main paint layer
-mainPath = carPath + "\\car_" + carNum;
-paintableLayer.visible = true;
-infoLayer.visible = false;
-specLayer.visible = false;
+if(paintableLayer != undefined) paintableLayer.visible = true;
+if(infoLayer != undefined) infoLayer.visible = false;
+if(specLayer != undefined) specLayer.visible = false;
+
 saveTarga(mainPath);
 
 // undo visibility changes
-paintableLayer.visible = paintableVisible;
-specLayer.visible = specVisible;
-infoLayer.visible = infoVisible;
+if(paintableLayer != undefined) paintableLayer.visible = paintableVisible;
+if(infoLayer != undefined) infoLayer.visible = infoVisible;
+if(specLayer != undefined) specLayer.visible = specVisible;
 
-function saveTarga(filepath) {
+function saveTarga(filePath) {
     targaSaveOptions = new TargaSaveOptions();
     targaSaveOptions.alphaChannels = true;
     targaSaveOptions.resolution = TargaBitsPerPixels.THIRTYTWO;
-    app.activeDocument.saveAs(File(filepath), targaSaveOptions, true, Extension.LOWERCASE);
+    app.activeDocument.saveAs(File(filePath), targaSaveOptions, true, Extension.LOWERCASE);
 };
